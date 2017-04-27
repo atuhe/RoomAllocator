af7a6284c1a7f6b25697e3493ab34efa84c24e9b
atuhe <ratuhe60@gmail.com>
2017-04-27 13:04:16 +0300

    Added tests for testrooms for maximum living room occupants

--- Tests/test_room.py
+++ Tests/test_room.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest import TestCase
-from Dojo_app.dojo import *
-from Dojo_app.rooms import *
+from app.person import *
+from app.rooms import *
 
 
 class Tests(unittest.TestCase):
@@ -10,12 +10,17 @@ class Tests(unittest.TestCase):
 
    def test_room_is_office(self):
         office1 = Office('blue')
-        self.assertEqual(office1.room_type, 'O')
+        self.assertEqual(office1.room_type, 'office')
 
+    #Test for maximum living_space occupants
    def test_max_living_space_occupants(self):
         ls1 = LivingSpace('green')
         self.assertEqual(ls1.max_occupants, 4)
 
+    #Test for if room is living_space
    def test_room_is_lspace(self):
         Space = LivingSpace('green')
-        self.assertEqual(Space.room_type, 'L')
+        self.assertEqual(Space.room_type, 'livingspace')
+
+
+
